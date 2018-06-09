fc908a78d71a055eb51bd1720d9340b08a4aa482
zhangchao <zc_820@163.com>
2018-06-09 17:32:45 +0800

    add Timeout method

--- demo/定时器/time-worker.js
+++ demo/定时器/time-worker.js
@@ -5,7 +5,7 @@ var intervalIds = {};
 // 监听message 开始执行定时器或者销毁
 self.onmessage = function(e){
     switch(e.data.command){
-        case 'interval:start': // 开启定时器
+        case 'interval:start':
             var intervalId = setInterval(function(){
                 postMessage({
                     message: 'interval:tick',
@@ -17,14 +17,37 @@ self.onmessage = function(e){
                 message: 'interval:started',
                 id: e.data.id
             });
-
             intervalIds[e.data.id] = intervalId;
+            break;
+        case 'timeout:start':
+            var timeoutId = setTimeout(function(){
+                postMessage({
+                    message: 'timeout:tick',
+                    id: e.data.id
+                })
+            },e.data.timeout);
+
+            postMessage({
+                message: 'timeout:started',
+                id: e.data.id
+            });
+            intervalIds[e.data.id] = timeoutId;
             break;
         case 'interval:clear': // 销毁
             clearInterval(intervalIds[e.data.id]);
 
             postMessage({
                 message: 'interval:cleared',
+                id: e.data.id
+            })
+
+            delete intervalIds[e.data.id];
+            break;
+        case 'timeout:clear': // 销毁
+            clearTimeout(intervalIds[e.data.id]);
+
+            postMessage({
+                message: 'timeout:cleared',
                 id: e.data.id
             })
 
--- demo/定时器/worker-timer.js
+++ demo/定时器/worker-timer.js
@@ -10,6 +10,13 @@ var workerTimer = {
         worker.postMessage({ command: 'interval:start', interval: interval, id: id });
         return id;
     },
+    setTimeout: function(cb, timeout, context) {
+        this.id++;
+        var id = this.id;
+        this.callbacks[id] = { fn: cb, context: context };
+        worker.postMessage({ command: 'timeout:start', timeout: timeout, id: id });
+        return id;
+    },
 
     // 监听worker 里面的定时器发送的message 然后执行回调函数
     onMessage: function(e) {
@@ -18,7 +25,14 @@ var workerTimer = {
                 var callback = this.callbacks[e.data.id];
                 if (callback && callback.fn) callback.fn.apply(callback.context);
                 break;
+            case 'timeout:tick':
+                var callback = this.callbacks[e.data.id];
+                if (callback && callback.fn) callback.fn.apply(callback.context);
+                break;
             case 'interval:cleared':
+                delete this.callbacks[e.data.id];
+                break;
+            case 'timeout:cleared':
                 delete this.callbacks[e.data.id];
                 break;
         }
@@ -27,6 +41,9 @@ var workerTimer = {
     // 往worker里面发送销毁指令
     clearInterval: function(id) {
         worker.postMessage({ command: 'interval:clear', id: id });
+    },
+    clearTimeout: function(id) {
+        worker.postMessage({ command: 'timeout:clear', id: id });
     }
 };
 
