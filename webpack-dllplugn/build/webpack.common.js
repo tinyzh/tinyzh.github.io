cbe14395f7ee93e74bd03491b9fa832e829499af
zhangchao <zhangchao@yunliketech.com>
2019-06-19 09:09:01 +0800

    upd

--- webpack-dllplugn/build/webpack.common.js
+++ webpack-dllplugn/build/webpack.common.js
@@ -1,8 +1,37 @@
 const path = require('path');
+const fs = require('fs')
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const webpack = require('webpack');
 const AddAssetHtmlWebpackPlugin = require('add-asset-html-webpack-plugin')
+
+const plugins = [
+	new HtmlWebpackPlugin({
+		template: 'src/index.html'
+	}),
+	new CleanWebpackPlugin(['dist'], {
+		root: path.resolve(__dirname, '../')
+	})
+]
+
+const files = fs.readdirSync(path.resolve(__dirname, '../dll'))
+files.forEach(file => {
+	if (/.*\.dll.js/.test(file)) {
+		plugins.push(
+			new AddAssetHtmlWebpackPlugin({
+				filepath: path.resolve(__dirname, '../dll', file)
+			})
+		)
+	}
+	if (/.*\.manifest.json/.test(file)) {
+		plugins.push(
+			new webpack.DllReferencePlugin({
+				manifest: path.resolve(__dirname, '../dll',file)
+			})
+		)
+	}
+})
+
 
 module.exports = {
 	entry: {
@@ -32,20 +61,7 @@ module.exports = {
 			}
 		}]
 	},
-	plugins: [
-		new HtmlWebpackPlugin({
-			template: 'src/index.html'
-		}),
-		new CleanWebpackPlugin(['dist'], {
-			root: path.resolve(__dirname, '../')
-		}),
-		new AddAssetHtmlWebpackPlugin({
-			filepath: path.resolve(__dirname, '../dll/vendors.dll.js')
-		}),
-		new webpack.DllReferencePlugin({
-			manifest: path.resolve(__dirname, '../dll/vendors.manifest.json')
-		})
-	],
+	plugins: plugins,
 	optimization: {
 		runtimeChunk: {
 			name: 'runtime'
--- webpack-dllplugn/build/webpack.dll.js
+++ webpack-dllplugn/build/webpack.dll.js
@@ -4,7 +4,8 @@ const webpack = require('webpack')
 module.exports = {
     mode: 'production',
     entry: {
-        vendors: ['react','react-dom','lodash']
+        vendors: ['lodash'],
+        react: ['react','react-dom']
     },
     output: {
         filename: '[name].dll.js',
